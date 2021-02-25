682ea1d1d4504a22785cba56c82908fa386be3d5
bhimasty <48201353+bhimasty@users.noreply.github.com>
2021-02-25 11:11:21 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Pasha Bhimasty" width="200px" class="rounded-circle shadow">
+  <p>Halo, namaku <?= $data['nama']; ?>. Aku berumur <?= $data['umur']; ?> tahun. Aku adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
